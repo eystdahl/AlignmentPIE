f3f84af9f3365c5a1c7cedf28f80e7fcf829a25c
eystdahl <93379583+eystdahl@users.noreply.github.com>
2022-06-16 08:49:33 +0200

    Add files via upload

---
+++ HomericData.R
@@ -0,0 +1,9 @@
+# Matrix of data from Iliad and Odyssey
+
+HomericData <- matrix(c(106, 37, 62, 25), ncol = 2, byrow = TRUE)
+colnames(HomericData) <- c("Singular", "Plural")
+rownames(HomericData) <- c("Iliad", "Odyssey")
+HomericData
+
+#Chisquared test
+
