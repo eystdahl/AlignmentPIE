eca4b5aab5d57569cc9956603eb735a5891ae4b3
eystdahl <93379583+eystdahl@users.noreply.github.com>
2022-06-24 17:14:59 +0200

    Add files via upload

--- DiachronicDataAncientGreek.R
+++ DiachronicDataAncientGreek.R
@@ -1,6 +1,6 @@
 #Create matrix of diachronic data from Ancient Greek
 
-GreekAlignmentDiachronic <- matrix(c(168, 25, 38, 50, 34, 62, 7, 3, 0, 0), ncol = 5, byrow = TRUE)
+GreekAlignmentDiachronic <- matrix(c(168, 25, 38, 50, 34, 62, 7, 3, 0, 2), ncol = 5, byrow = TRUE)
 colnames(GreekAlignmentDiachronic) <- c("Homer", "Hesiod", "Herodotus", "Thucydides", "Polybius")
 rownames(GreekAlignmentDiachronic) <- c("Singular agreement", "Plural agreement")
 
